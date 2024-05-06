4dac5a28c377ae24f3e599cb1fb6cfe2da7e1f93
Abhinav Jain <jabhinav041@gmail.com>
2024-05-06 12:43:45 +0530

    added winner logic , moved main game logic to app.jsx from board.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import Board from './components/Board';
 import './styles.scss';
+import { useState } from 'react';
+import { calculateWinner } from './winner';
+
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `winner is ${winner}`
+    : `next player is ${nextPlayer}`;
+
+  const handlesquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (position == clickedPosition) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handlesquareClick={handlesquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,7 @@
 import Square from './Square';
 import { useState } from 'react';
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handlesquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (position == clickedPosition) {
-          return isXNext ? 'X' : 'O';
-        }
-        return squareValue;
-      });
-    });
-    setIsXNext(prev => !prev);
-  };
-
+function Board({ squares, handlesquareClick }) {
   const renderSquare = position => {
     return (
       <Square
